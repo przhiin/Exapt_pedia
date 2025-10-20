6758cae0a8cb42c565347fa653d2388554c1ef46
przhiin <preshinraj7@gmail.com>
2025-10-20 16:50:12 +0530

    Delete EVENTS directory

--- EVENTS/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Events
-
-admin.site.register(Events)
--- EVENTS/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Events
-from .serializers import EventSerializer
-from rest_framework import viewsets
-
-
-class EventViewSets(viewsets.ModelViewSet):
-    queryset = Events.objects.all()
-    serializer_class =  EventSerializer
